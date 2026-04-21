51724ec661ba81e27b13eac8452b50c31907bd51
huylebron <doquochuy27102003@gmail.com>
2026-04-21 23:19:24 +0700

    Initial project setup with Spring Boot, Maven wrapper, and PostgreSQL configuration

---
+++ src/main/java/com/luma/lumacoursesv2/LumaCoursesV2Application.java
@@ -0,0 +1,14 @@
+package com.luma.lumacoursesv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumaCoursesV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LumaCoursesV2Application.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/luma/lumacoursesv2/LumaCoursesV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luma.lumacoursesv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumaCoursesV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
